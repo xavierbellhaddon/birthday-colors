199bcc47551bda10d599d1d25a80d7dd8cf53f18
Austin Bell <snarkeet@gmail.com>
2021-11-20 03:38:46 -0500

    restyled dropdown section

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const background = document.querySelector("body");
+const body = document.querySelector("body");
 const container = document.getElementById("container");
-const coloredText = document.getElementById("coloredText")
 const outputFirstLine = document.querySelector("#colorOutput p:first-child");
 const hex = document.getElementById("hex");
 const outputLastLine = document.querySelector("#colorOutput p:last-child");
 // const hex = document.querySelector(".color-hex");
 // const paletteBlock = document.querySelector(".palette-block");
 const colorOutput = document.getElementById("colorOutput");
-const colorBlock = document.querySelector(".color-block");
-const paletteMenu = document.querySelector(".palette-menu");
+// const colorBlock = document.querySelector(".color-block");
+const paletteMenu = document.getElementById("paletteMenu");
 
 function getColor(month, day, hex) {
     const displayColor = document.querySelector(".color");
@@ -24,11 +23,11 @@ function getColor(month, day, hex) {
         const json = JSON.parse(spectrum.responseText);
         const color = json.records[day].color;
 
-        background.classList.add("recolored");
-        background.style.backgroundColor = color;
+        body.classList.add("recolored");
+        body.style.backgroundColor = color;
 
-        coloredText.classList.add("recolored");
-        coloredText.style.color = color;
+        // coloredText.classList.add("recolored");
+        // coloredText.style.color = color;
         
         colorOutput.style.height = "322px";
         colorOutput.style.margin = "2rem 0 2rem 0";
@@ -42,7 +41,7 @@ function getColor(month, day, hex) {
         
         outputLastLine.classList.add("displayed");
 
-        container.style.height = "668.5px";
+        // container.style.height = "668.5px";
         
         heading.forEach(function (element) {
             element.style.color = color;
@@ -53,11 +52,11 @@ function getColor(month, day, hex) {
 function getPalettes(baseColor) {
     const paletteModes = document.querySelector(".palette-modes");
     const mode = paletteModes.options[paletteModes.selectedIndex].value;
-    const one = document.querySelector(".one");
-    const two = document.querySelector(".two");
-    const three = document.querySelector(".three");
-    const four = document.querySelector(".four");
-    const five = document.querySelector(".five");
+    const colorOne = document.getElementById("colorOne");
+    const colorTwo = document.getElementById("colorTwo");
+    const colorThree = document.getElementById("colorThree");
+    const colorFour = document.getElementById("colorFour");
+    const colorFive = document.getElementById("colorFive");
     const paletteURL =
         "https://www.thecolorapi.com/scheme?hex=" +
         baseColor +
@@ -69,17 +68,17 @@ function getPalettes(baseColor) {
     palette.send();
     palette.onload = function () {
         const colors = JSON.parse(palette.responseText).colors;
-        one.style.backgroundColor = colors[0].hex.value;
-        two.style.backgroundColor = colors[1].hex.value;
-        three.style.backgroundColor = colors[2].hex.value;
-        four.style.backgroundColor = colors[3].hex.value;
-        five.style.backgroundColor = colors[4].hex.value;
+        colorOne.style.backgroundColor = colors[0].hex.value;
+        colorTwo.style.backgroundColor = colors[1].hex.value;
+        colorThree.style.backgroundColor = colors[2].hex.value;
+        colorFour.style.backgroundColor = colors[3].hex.value;
+        colorFive.style.backgroundColor = colors[4].hex.value;
 
-        one.innerText = colors[0].hex.value;
-        two.innerText = colors[1].hex.value;
-        three.innerText = colors[2].hex.value;
-        four.innerText = colors[3].hex.value;
-        five.innerText = colors[4].hex.value;
+        colorOne.innerText = colors[0].hex.value;
+        colorTwo.innerText = colors[1].hex.value;
+        colorThree.innerText = colors[2].hex.value;
+        colorFour.innerText = colors[3].hex.value;
+        colorFive.innerText = colors[4].hex.value;
 
         paletteBlock.style.display = "block";
     };
